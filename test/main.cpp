54f33fdb17fde82fbe813d132d8f3a2781c75a25
zishirs <zishirs@dhcp-10-18-62-136.dhcp.nd.edu>
2012-07-03 14:34:49 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by zishirs on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
